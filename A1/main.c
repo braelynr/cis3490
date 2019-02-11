1f89c2d86e63cb15ea63e6f601baa0ff541ae1b6
socs <socs@socsvm>
2019-02-10 23:22:31 -0500

    finished 2.1

--- A1/main.c
+++ A1/main.c
@@ -6,16 +6,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct point{
+  float x;
+  float y;
+} Point;
+
 void findInvBrute(int *set, int n);
 void findInvMerge(int *set, int n);
+void convexBrute(Point *plane, int n);
 
 int main(void)
 {
   FILE *fp;
   int *set;
+  Point *plane;
   int i = 0;
   int size = 50;
-
+/*
   set = calloc(50, sizeof(int));
 
   fp = fopen("data_1.txt", "r");
@@ -34,13 +41,39 @@ int main(void)
     }
   }
   fclose(fp);
-  /*for(int j = 0 ; j < i ; j++)
-  {
-    printf("%d, ", set[j]);
-  }*/
-  //findInvBrute(set, i);
-  //findInvMerge(set, i);
+
+  findInvBrute(set, i);
+  findInvMerge(set, i);
   free(set);
+  */
+
+  size = 50;
+  plane = calloc(50, sizeof(Point));
+
+  fp = fopen("data.txt", "r");
+  if(fp == NULL)
+  {
+    printf("Failed to open file \"data_2.txt\"\n");
+    return -1;
+  }
+  while(fscanf(fp, "%f %f", &plane[i].x, &plane[i].y) != EOF)
+  {
+    i++;
+    if(size == i + 1)
+    {
+      plane = realloc(plane, sizeof(Point)*(size + 50));
+      size = size + 50;
+    }
+  }
+  fclose(fp);
+
+  /*for (int i = 0 ; i < 6; i++)
+  {
+    printf("%f %f\n", plane[i].x, plane[i].y);
+  }*/
+
+  convexBrute(plane, i);
+  free(plane);
 
   return 0;
 }
